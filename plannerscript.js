53d762321f521dde3c27b4cf33554330dadf2498
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-09-09 02:15:35 -0500

    finalizing divs

--- plannerscript.js
+++ plannerscript.js
@@ -7,29 +7,27 @@
 
 
 function buildTimeBlockElements() {
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
     for (var i = 09; i < 18; i++) {
-        if (i = 12){
-            var divContainer = ($("<div></div>").attr("id", "hour-" + i).addClass("row")).append($("<div>"+ "hour" + "</div>").addClass("hour").text((i) + "PM"))
+        if (i < 12) {
+            var divContainer = ($("<div></div>").attr("id", "hour-" + i).addClass("row")).append($("<div>" + "hour" + "</div>").addClass("hour").text(i + "AM"))
             $(".container").append(divContainer)
-        }
+        } else {
+            if (i === 12) {
+                var divContainer = ($("<div></div>").attr("id", "hour-" + i).addClass("row")).append($("<div>" + "hour" + "</div>").addClass("hour").text((i) + "PM"))
+                $(".container").append(divContainer)
+            } else {
+                var divContainer = ($("<div></div>").attr("id", "hour-" + i).addClass("row")).append($("<div>" + "hour" + "</div>").addClass("hour").text((i - 12) + "PM"))
+                $(".container").append(divContainer)
+            }
 
-        if (i < 12){
-        var divContainer = ($("<div></div>").attr("id", "hour-" + i).addClass("row")).append($("<div>"+ "hour" + "</div>").addClass("hour").text(i + "AM"))
-        $(".container").append(divContainer)
         }
-        else{
-            var divContainer = ($("<div></div>").attr("id", "hour-" + i).addClass("row")).append($("<div>"+ "hour" + "</div>").addClass("hour").text((i - 12) + "PM"))
-            $(".container").append(divContainer)
+    }
 
-        }
-        // else {
-            // var divContainer = ($("<div></div>").attr("id", "hour-" + i).addClass("row")).append($("<div>"+ "hour" + "</div>").addClass("hour").text(24 - i))
-        // }
-        // rowContainer.append($("<div></div>").addClass("hour"));
-    }}
-    // $(".row").append($("<div>"+ "hour" + "</div>").addClass("hour"));
-    // $(".row").append($("<div>"+ "hour" + "</div>").addClass("description"));
-    // $(".row").append("<input type='image' id='image' src='../assets/save_icon.png'>");
+    $(".row").append($("<div>" + "hour" + "</div>").addClass("description"));
+    $(".row").append("<input type='image' class ='saveBtn' id='image' src='assets/save_icon.png'></input>");
+}
 
 
 $(document).ready(function () {
