e92db9a6792cab23baf7fc21ca7872e4824bd489
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-09-09 00:41:53 -0500

    beginning dev

--- plannerscript.js
+++ plannerscript.js
@@ -5,20 +5,34 @@
 // }
 // span naturally on right, inline 
 
+
 function buildTimeBlockElements() {
     for (var i = 09; i < 18; i++) {
-    // timeblockElement.append(newDiv.attr("id", "hour-" + i));
-    $(".container").append($("<div></div>").attr("id", "hour-" + i))
-    
-    
+        if (i = 12){
+            var divContainer = ($("<div></div>").attr("id", "hour-" + i).addClass("row")).append($("<div>"+ "hour" + "</div>").addClass("hour").text((i) + "PM"))
+            $(".container").append(divContainer)
+        }
+
+        if (i < 12){
+        var divContainer = ($("<div></div>").attr("id", "hour-" + i).addClass("row")).append($("<div>"+ "hour" + "</div>").addClass("hour").text(i + "AM"))
+        $(".container").append(divContainer)
+        }
+        else{
+            var divContainer = ($("<div></div>").attr("id", "hour-" + i).addClass("row")).append($("<div>"+ "hour" + "</div>").addClass("hour").text((i - 12) + "PM"))
+            $(".container").append(divContainer)
+
+        }
+        // else {
+            // var divContainer = ($("<div></div>").attr("id", "hour-" + i).addClass("row")).append($("<div>"+ "hour" + "</div>").addClass("hour").text(24 - i))
+        // }
+        // rowContainer.append($("<div></div>").addClass("hour"));
+    }}
+    // $(".row").append($("<div>"+ "hour" + "</div>").addClass("hour"));
+    // $(".row").append($("<div>"+ "hour" + "</div>").addClass("description"));
+    // $(".row").append("<input type='image' id='image' src='../assets/save_icon.png'>");
 
 
-    }
-
-}
-
-
-$(document).ready(function() {
+$(document).ready(function () {
     // getLocalStorage()
     buildTimeBlockElements()
     // savingTasks()
