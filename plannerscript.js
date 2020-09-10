aa2cac21a4f29bf55f0f77a933b6dafaa6c7cb23
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-09-09 20:16:00 -0500

    adding in clicking on save button and unit testing coloring

--- plannerscript.js
+++ plannerscript.js
@@ -1,9 +1,27 @@
-// make one time slot in index.html then program it 
+function buildTimeBlockElements() {
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-// for (var i = 09 ; i < 18; i++){
+    for (var i = 09; i < 18; i++) {
+        if (i < 12) {
+            var divContainer = ($("<div></div>").attr("id", "hour-" + i).addClass("row")).append($("<div>" + "hour" + "</div>").addClass("hour").text(i + "AM"));
+            $(".container").append(divContainer);
+        } else {
+            if (i === 12) {
+                var divContainer = ($("<div></div>").attr("id", "hour-" + i).addClass("row")).append($("<div>" + "hour" + "</div>").addClass("hour").text((i) + "PM"));
+                $(".container").append(divContainer);
+            } else {
+                var divContainer = ($("<div></div>").attr("id", "hour-" + i).addClass("row")).append($("<div>" + "hour" + "</div>").addClass("hour").text((i - 12) + "PM"));
+                $(".container").append(divContainer);
+            }
 
-// }
-// span naturally on right, inline 
+        }
+    }
+
+    $(".row").append($("<input>" + "</input>").addClass("description"));
+    $(".row").append("<input type='image' class ='saveBtn' id='image' src='assets/save_icon.png'></input>");
+    // getLocalStorage();
+    assigningColor();
+}
 
 function getLocalStorage() {
     var savedTasks = JSON.parse(localStorage.getItem("savedTasks"));
@@ -12,67 +30,47 @@ function getLocalStorage() {
         localStorage.setItem("savedTasks", {});
     }
 
-    console.log(savedTasks)
+    console.log(savedTasks);
 
     // localStorage.setItem("scores", JSON.stringify(new_array));
 
 }
 
-function buildTimeBlockElements() {
-    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+function assigningColor() {
 
-    for (var i = 09; i < 18; i++) {
-        if (i < 12) {
-            var divContainer = ($("<div></div>").attr("id", "hour-" + i).addClass("row")).append($("<div>" + "hour" + "</div>").addClass("hour").text(i + "AM"))
-            $(".container").append(divContainer)
-        } else {
-            if (i === 12) {
-                var divContainer = ($("<div></div>").attr("id", "hour-" + i).addClass("row")).append($("<div>" + "hour" + "</div>").addClass("hour").text((i) + "PM"))
-                $(".container").append(divContainer)
-            } else {
-                var divContainer = ($("<div></div>").attr("id", "hour-" + i).addClass("row")).append($("<div>" + "hour" + "</div>").addClass("hour").text((i - 12) + "PM"))
-                $(".container").append(divContainer)
-            }
+    var currentTime = moment().format("H a");
+    var currentHour = ((currentTime.replace(" ", ""))[0] + (currentTime.replace(" ", ""))[1].toUpperCase() + (currentTime.replace(" ", ""))[2].toUpperCase());
+    var currentHourNumber = parseInt(currentTime.split(" ")[0] - 3);
 
-        }
+    var divClass = "#" + "hour-" + (currentHourNumber);
+
+    var currentHourDiv = $(divClass).children("input.description").addClass("present");
+
+    for (i = 09; i < currentHourNumber; i++) {
+        $("#" + "hour-" + (i)).children("input.description").addClass("past");
+
     }
 
-    $(".row").append($("<input>" + "</input>").addClass("description").addClass("past"));
-    $(".row").append("<input type='image' class ='saveBtn' id='image' src='assets/save_icon.png'></input>");
-
-    assigningColor()
-
-    savingEntries()
-
-}
-
-function assigningColor() {
-    var divRows = $(".hour").textConent
-    // console.log(divRows.split("M"))
-
-    var currentTime = moment().format("h a")
-    var currentHour = ( (currentTime.replace(" ", ""))[0]+ (currentTime.replace(" ", ""))[1].toUpperCase() + (currentTime.replace(" ", ""))[2].toUpperCase())
-
-
-    console.log(divRows)
-        
+    for (i = 18; i > currentHourNumber; i--) {
+        $("#" + "hour-" + (i)).children("input.description").addClass("future")
+    };
 
 
 
 }
 
 function savingEntries() {
+    var divRows = $(".rows").text();
 
-    var divRows = $(".rows").text()
+    console.log("clicked save")
 
     // for (divs in divRows){
     //     console.log(divRows[divs])
     // }
 }
 
-
 $(document).ready(function () {
-    // getLocalStorage()
     buildTimeBlockElements()
-    // savingTasks()
+
+    $(".saveBtn").on("click", savingEntries);
 });
